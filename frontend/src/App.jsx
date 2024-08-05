86c212a6ac8ea556a53911a4d05851659f7953eb
Jayesh Pingale <JayeshPingale@users.noreply.github.com>
2024-08-06 01:16:23 +0530

    day 1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+// App.js
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import FileUpload from './FileUpload';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
